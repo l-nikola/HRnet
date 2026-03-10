cb7254c4763cd8f0a9db0d7b7d5727fd4d311c23
l-nikola <legay.nikola.pro@gmail.com>
2026-03-10 20:12:12 +0100

    feat : EmployeeAddress + EmployeeDepartment style

--- src/components/Form/EmployeeDepartment.jsx
+++ src/components/Form/EmployeeDepartment.jsx
@@ -8,21 +8,23 @@ export default function EmployeeDepartment({ state, handleChange }) {
   return (
     <section className="employeeDepartment">
       <h3>Department</h3>
-      <FormControl variant="standard" fullWidth>
-        <InputLabel id="departement-label">Department</InputLabel>
-        <Select
-          labelId="departement-label"
-          value={state}
-          onChange={handleChange}
-        >
-          <MenuItem value="Sale">Sale</MenuItem>
-          <MenuItem value="Marketing">Marketing</MenuItem>
-          <MenuItem value="Engineering">Engineering</MenuItem>
-          <MenuItem value="HumanResources">Human Resources</MenuItem>
-          <MenuItem value="Legal">Legal</MenuItem>
-        </Select>
-      </FormControl>
-      <TextField label="Zip Code" type="number" variant="standard" />
+      <div>
+        <FormControl variant="standard" fullWidth>
+          <InputLabel id="departement-label">Department</InputLabel>
+          <Select
+            labelId="departement-label"
+            value={state}
+            onChange={handleChange}
+          >
+            <MenuItem value="Sale">Sale</MenuItem>
+            <MenuItem value="Marketing">Marketing</MenuItem>
+            <MenuItem value="Engineering">Engineering</MenuItem>
+            <MenuItem value="HumanResources">Human Resources</MenuItem>
+            <MenuItem value="Legal">Legal</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField label="Zip Code" type="number" variant="standard" />
+      </div>
     </section>
   );
 }
