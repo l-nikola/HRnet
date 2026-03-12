73ae2898699ceb54eb2a1ec51a79f1f9356dee70
l-nikola <legay.nikola.pro@gmail.com>
2026-03-12 12:45:07 +0100

    feat : setup redux persist

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 import CreateEmployee from "./pages/CreateEmployee/";
 import CurrentEmployee from "./pages/CurrentEmployee/";
 import Error from "./pages/Error/";
@@ -10,13 +11,15 @@ import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<CreateEmployee />} />
-        <Route path="/current-employee" element={<CurrentEmployee />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </Router>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<CreateEmployee />} />
+          <Route path="/current-employee" element={<CurrentEmployee />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    </PersistGate>
   </Provider>,
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import employeeReducer from "./slices/employeeSlice";
 
+const persistConfig = { key: "root", storage };
+const persistedReducer = persistReducer(persistConfig, employeeReducer);
+
 export const store = configureStore({
-  reducer: { employees: employeeReducer },
+  reducer: { employees: persistedReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
+
+export const persistor = persistStore(store);
